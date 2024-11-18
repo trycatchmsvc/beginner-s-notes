f6f81082cfa72b8e6b27c30b9ec63b8163bcc2ab
Beginner python <anton2006220809542@gmail.com>
2024-11-18 22:27:21 +0700

    Update Z28.cpp

--- Tasks Folder/Z28.cpp
+++ Tasks Folder/Z28.cpp
@@ -5,14 +5,30 @@
 using namespace std;
 
 void rstrip(char* str, const char* str_2);
+size_t strLen(string str);
 
 int main() {
 
-	char s[] = "All work and no play makes Jack a dull boy :))";
+	string s = "All work and no play makes Jack a dull boy: g) a)";
 
-	char chars[] = ":))";
+	string chars = ":))";
 
-	rstrip(s, chars);
+	int size_s = (int)strLen(s);
+	int size_chars = (int)strLen(chars);
+	
+	//cout << size_chars;
+
+	int pointer = 0;
+	string str_val = "";
+	string str_arr[255] = {};
+	
+	for (int i = 0; i < size_chars; i++) { 
+		for (int j = (int)s.length() - 1; j >= 0; j--) { 
+			if (s[j] == chars[i]) {             
+				s.erase(j, 1);                    
+			}
+		}
+	}
 
 	cout << s << endl;
 
@@ -20,7 +36,7 @@ int main() {
 }
 
 void rstrip(char* str, const char* str_2) {
-	for (int i{(int)strlen(str) }; i >= strlen(str) - strlen(str_2); i--) {
+	for (int i{ (int)strlen(str) }; i >= strlen(str) - strlen(str_2); i--) {
 		for (int j{ 0 }; j < strlen(str_2); j++) {
 			if (str[i] == str_2[j]) {
 				str[i] = ' ';
@@ -28,4 +44,14 @@ void rstrip(char* str, const char* str_2) {
 		}
 
 	}
+}
+
+size_t strLen(const string str) { //Realise func strlen
+	size_t len = 0;
+
+	for (int i{ 0 }; str[i] != '\0'; i++) {
+		len++;
+	}
+
+	return len;
 }
