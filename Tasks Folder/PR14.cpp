9bd4597bfdfb9a2d546a90b97d4794571efe94f3
Beginner python <anton2006220809542@gmail.com>
2024-12-18 23:55:08 +0700

    Update PR14.cpp
    Добавлена реализация календаря(только високосного года)

--- Tasks Folder/PR14.cpp
+++ Tasks Folder/PR14.cpp
@@ -25,3 +25,72 @@ int* get_date(const string &date) {
 	int arr[3] = { day, month, year };
 	return arr;
 }
+
+	int days = 28;
+	int month = 11;
+	int year = 2024;
+	int plus_days = 4;
+
+	cout << "day: " << days << endl << "month: " << month << endl << "year: " << year << endl;
+
+	int day = days + plus_days;
+
+	while (plus_days != 0) {
+		if (year % 4 == 0) { // смотрим четность года для определения високосного года
+			if (month <= 7) {
+				if (month % 2 != 0 && month != 2) {
+					if (day > 31) {
+						day = day - 31;
+						plus_days = (plus_days - (31 - days)) - day;
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+				else if (month == 2) {
+					if (day > 29) {
+						day = day - 29;
+						plus_days = (plus_days - (29 - days)) - day;
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+					
+				}
+				else if (month % 2 == 0 && month != 2) {
+					if (day > 30) {
+						day = day - 30;
+						plus_days = (plus_days - (30 - days)) - day;
+						month++; 
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+			}
+			else if (month >= 8){
+				if (month % 2 == 0) {
+					if (day > 31) {
+						day = day - 31;
+						plus_days = (plus_days - (31 - days)) - day;
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+				else {
+					if (day > 30) {
+						day = day - 30;
+						plus_days = (plus_days - (30 - days)) - day;
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+			}
+		}
+	}
