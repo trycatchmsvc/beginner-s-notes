ccc6762e890cbfbe931e4088d2849f2d07392036
Beginner python <anton2006220809542@gmail.com>
2024-12-20 01:07:32 +0700

    Realise 14 practic tesk
    Выполнение 14 практической работы:
    
    Выполнение задания оформить как функцию void my_task(std::istream &ist, std::ostream &ost), которая вводит данные из потока ist и выводит в поток ost. Использование данных типов параметров обусловлено тем, что с типом istream совместимы как cin, так и файловые потоки ввода ifstream, с типом ostream совместимы как cout, так и файловые потоки вывода ofstream.
    
    Разработать программу, которая вводит данные из текстового файла, преобразует их в соответствии с вариантом из таблицы, а затем выводит результат в другой текстовый файл.
    
    В каждой строке исходного текста содержится корректно заданная дата в формате «dd.mm.yy», где dd — день, mm — месяц, yy — год. Получить строку с датой, отстоящей от заданной на n дней вперёд, в том же формате

--- Tasks Folder/PR14.cpp
+++ Tasks Folder/PR14.cpp
@@ -1,108 +1,24 @@
+//В каждой строке исходного текста содержится корректно заданная дата в формате «dd.mm.yy», где dd — день, mm — месяц, 
+// yy — год. Получить строку с датой, отстоящей от заданной на n дней вперёд, в том же формате
 #include <iostream>
 #include <string>
 #include <fstream>
 using namespace std;
 
 int* get_date(const string &date);
+void my_task(std::istream& ist, std::ostream& ost);
 
 int main() {
 
-	//string inp_path = "input.txt";
-	//string out_path = "output.txt";
-	//ifstream ist(inp_path);
-	//ofstream ost(out_path);
+	string inp_path = "input.txt";
+	string out_path = "output.txt";
+	ifstream ist(inp_path);
+	ofstream ost(out_path);
 
-	string date = "18.12.2024";
+	my_task(ist, ost);
 
-	int days = 28;
-	int month = 11;
-	int year = 2024;
-	int plus_days = 4;
-
-	cout << "day: " << days << endl << "month: " << month << endl << "year: " << year << endl;
-
-	int day = days + plus_days;
-
-	while (plus_days != 0) {
-
-			if (month <= 7) {
-				if (month % 2 != 0 && month != 2) {
-					if (day > 31) {
-						day = day - 31;
-						plus_days = plus_days - (31 - days);
-						month++;
-					}
-					else {
-						plus_days = 0;
-					}
-				}
-				else if (month == 2 and year % 4 == 0) {
-					if (day > 29) {
-						day = day - 29;
-						plus_days = plus_days - (29 - days);
-						month++;
-					}
-					else {
-						plus_days = 0;
-					}
-					
-				}
-				else if (month == 2 and year % 4 != 0) {
-					if (day > 28) {
-						day = day - 28;
-						plus_days = plus_days - (29 - days);
-						month++;
-					}
-					else {
-						plus_days = 0;
-					}
-
-				}
-				else if (month % 2 == 0 && month != 2) {
-					if (day > 30) {
-						day = day - 30;
-						plus_days = plus_days - (30 - days);
-						month++; 
-					}
-					else {
-						plus_days = 0;
-					}
-				}
-			}
-			else if (month >= 8){
-				if (month % 2 == 0) {
-					if (day > 31) {
-						day = day - 31;
-						plus_days = plus_days - (31 - days);
-						month++;
-					}
-					else {
-						plus_days = 0;
-					}
-				}
-				else {
-					if (day > 30) {
-						day = day - 30;
-						plus_days = plus_days - (30 - days);
-						month++;
-					}
-					else {
-						plus_days = 0;
-					}
-				}
-			}
-			if (month > 12) {
-				year++;
-				month = 1;
-			}
-	}
-
-	cout << day << endl << month << endl << year << endl;
-
-	
-
-	/*ist.close();
-	ost.close();*/
+	ist.close();
+	ost.close();
 	return 0;
 }
 
@@ -132,4 +48,98 @@ int* get_date(const string &date) {
 	}
 	int arr[3] = { day, month, year };
 	return arr;
+}
+
+void my_task(std::istream& ist, std::ostream& ost) {
+	char buffer[100] = "";
+	
+	while (ist.getline(buffer, 100)) {
+		
+		string date = string(buffer);
+		
+		int days = get_date(date)[0];
+		int month = get_date(date)[1];
+		int year = get_date(date)[2];
+		
+		int plus_days = 40;
+
+		cout << "day: " << days << endl << "month: " << month << endl << "year: " << year << endl;
+
+		int day = days + plus_days;
+
+		while (plus_days != 0) {
+
+			if (month <= 7) {
+				if (month % 2 != 0 && month != 2) {
+					if (day > 31) {
+						day = day - 31;
+						plus_days = plus_days - (31 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+				else if (month == 2 and year % 4 == 0) {
+					if (day > 29) {
+						day = day - 29;
+						plus_days = plus_days - (29 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+
+				}
+				else if (month == 2 and year % 4 != 0) {
+					if (day > 28) {
+						day = day - 28;
+						plus_days = plus_days - (29 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+
+				}
+				else if (month % 2 == 0 && month != 2) {
+					if (day > 30) {
+						day = day - 30;
+						plus_days = plus_days - (30 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+			}
+			else if (month >= 8) {
+				if (month % 2 == 0) {
+					if (day > 31) {
+						day = day - 31;
+						plus_days = plus_days - (31 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+				else {
+					if (day > 30) {
+						day = day - 30;
+						plus_days = plus_days - (30 - days);
+						month++;
+					}
+					else {
+						plus_days = 0;
+					}
+				}
+			}
+			if (month > 12) {
+				year++;
+				month = 1;
+			}
+		}
+		ost << day << '.' << month << '.' << year << endl;
+	}
 }
