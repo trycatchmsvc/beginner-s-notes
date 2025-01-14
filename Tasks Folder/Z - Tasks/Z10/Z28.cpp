a1da4c1c02fc012fd077859a6ca7f9be119e31a0
study c plus plus <anton2006220809542@gmail.com>
2025-01-15 00:06:01 +0700

    Update Z28.cpp
    Было изменено использование типа данных string на тип данных char
    -Была реализована функция по смещению строки

--- Tasks Folder/Z - Tasks/Z10/Z28.cpp
+++ Tasks Folder/Z - Tasks/Z10/Z28.cpp
@@ -1,44 +1,36 @@
 //28. Написать функцию void rstrip(char *s, const char *chars), которая удаляет
 // из конца строки s символы, входящие в строку chars.
-
 #include <iostream>
 using namespace std;
 
-void rstrip(string &s, const string chars);
-size_t strLen(string str);
+void rstrip(char* array, const char* del_symbols); // Функция опредялет схожесть символов и использует функцию displacement
+void displacement(char* array, const int &index); // Функция смещает всю строку начиная с index
 
 int main() {
 
-	string s = "All work and no play makes Jack a dull boy: g) a)";
+	char del_str[] = "));";
+	char str[] = "Hello world any )where) hold;";
+	
+	rstrip(str, del_str);
 
-	string chars = ":))";
-
-	rstrip(s, chars);
-
-	cout << s << endl;
+	cout << str << endl;
 
 	return 0;
 }
 
-void rstrip(string& s, const string chars) {
-	int size_s = (int)strLen(s);
-	int size_chars = (int)strLen(chars);
-
-	for (int i = 0; i < size_chars; i++) {
-		for (int j = strLen(s) - 1; j >= 0; j--) {
-			if (s[j] == chars[i]) {
-				s.erase(j, 1);
+void rstrip(char* array, const char *del_symbols) {
+	for (int j{ 0 }; del_symbols[j] != '\0'; j++) {
+		for (int i{ (int)strlen(array) }; i != 0; i--) {
+			if (array[i] == del_symbols[j]) {
+				displacement(array, i);
+				break;
 			}
 		}
 	}
 }
 
-size_t strLen(const string str) {
-	size_t len = 0;
-
-	for (int i{ 0 }; str[i] != '\0'; i++) {
-		len++;
+void displacement(char* array, const int &index) {
+	for (int i{index}; array[i] != '\0'; i++) {
+		array[i] = array[i + 1];
 	}
-
-	return len;
 }
