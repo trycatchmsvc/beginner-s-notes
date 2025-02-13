be558d1ef506244c0089d555f695a7230f1768c3
study c plus plus <anton2006220809542@gmail.com>
2025-02-13 23:41:48 +0700

    Update PR (15).cpp
    Реализована основная часть задания

--- Tasks Folder/Practic - Tasks/PR (15).cpp
+++ Tasks Folder/Practic - Tasks/PR (15).cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void read_array(int** &array, int const &lines, int const &columns);
-void text2bin(istream &ist, ostream &ost);
+void text2bin(ifstream &fin, ofstream &fout);
 
 int main(){
 
@@ -20,13 +20,44 @@ int main(){
         cout << "File was opened in all streams" << endl;
     }
 
+    text2bin(fin, fout);
+
+    fin.close();
+    fout.close();
+
+    if (!fin.is_open() && !fout.is_open()){
+        cout << "File was closed in all streams" << endl;
+    }
+    
+
+    //задание 
+
+
+
+    return 0;
+}
+
+void read_array(int** &array, int const &lines, int const &columns){
+    for(int i{0}; i < lines; i++){
+        for (int j{0}; j <= columns - 1; j++){
+            cout << array[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+void text2bin(ifstream &fin, ofstream &fout){
+
     char* temp = new char[255]; // для гетлайна
     int lines, columns;
     fin.getline(temp, 255); // получаем строку с lines и columns
 
-    lines = atoi(strtok(temp, " ")); // получаем первое число с строки
-    columns = atoi(strtok(nullptr, " ")); // получаем второе число с strtok
-
+    //записываем параметры матрицы
+    int parametrs_matrix[2]{atoi(strtok(temp, " ")), atoi(strtok(nullptr, " "))};
+    lines = parametrs_matrix[0];
+    columns = parametrs_matrix[1];
+    
+    //выводим параметры матрицы
     cout << lines << " " << columns << endl;
 
     //создание массива указателей
@@ -46,17 +77,21 @@ int main(){
         }
     }
 
+    // переворачиваем массив
+    for (int i{ 0 }; i < lines; i++) {
+		for (int j(0); j < columns / 2; j++) {
+			swap(array[i][j], array[i][columns - j - 1]);
+		}
+	}
+    
     //чтение массива
     read_array(array, lines, columns);
 
-
-
-
-
-
-    fin.close();
-    fout.close();
-
+    //запись массива в бинарный файл
+    fout.write((char *)&parametrs_matrix, sizeof(int) * 2);
+    for (int i{lines - 1}; i >= 0; i--){
+        fout.write((char *)array[i], sizeof(int) * columns);
+    }
 
     for (int i{0}; i < lines; i++){
         delete []array[i];
@@ -66,15 +101,4 @@ int main(){
     delete []array;
     array = nullptr;
 
-    return 0;
-}
-
-
-void read_array(int** &array, int const &lines, int const &columns){
-    for(int i{lines - 1}; i >= 0; i--){
-        for (int j{columns - 1}; j >= 0; j--){
-            cout << array[i][j] << ' ';
-        }
-        cout << endl;
-    }
 }
