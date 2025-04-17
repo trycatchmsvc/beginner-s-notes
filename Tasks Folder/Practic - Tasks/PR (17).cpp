a702bedb809174eb12f59ef995f5947ad3d0cd2d
student <anton2006220809542@gmail.com>
2025-04-18 01:24:35 +0700

    Update PR (17).cpp
    upd: fix

--- Tasks Folder/Practic - Tasks/PR (17).cpp
+++ Tasks Folder/Practic - Tasks/PR (17).cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <cstdlib>
+#include <cctype>
 
 using namespace std;
 
@@ -15,143 +17,134 @@ struct node_ {
 
 typedef node_ node;
 
-//own
+// own
 void push_back(node*& top, const char* symbol);
-void print_list_own(node* head_list);
+void print_list_own(node* head_list, fstream& ost);
 void push_forward(node*& top, const char* symbol);
 void fill_list(fstream& ist, node*& head);
 node* lstchr(node* head, const char* symbol);
 
-//task
+// task
 node* insert_node(node* head, node* prev, node* n);
 void print_list(const node* first, fstream& ost);
-bool is_even_digit_sum(const char* str);
+int is_even_digit_sum(const char* str);
 int generate_random_int(int min, int max);
-void process_list(node* head);
+void process_list(node*& head);
 
 
 int main() {
-
-    node* top = nullptr; //create head of list
+    node* top = nullptr;
     fstream ist, ost;
     ist.open("input.txt", ios_base::in);
     ost.open("output.txt", ios_base::out);
 
+    fill_list(ist, top);
 
-    fill_list(ist, top);   
 
-    print_list(top, ost);
-    print_list_own(top);
     process_list(top);
-    print_list_own(top);
-    
-
+    print_list(top, ost);
 
     ist.close();
     ost.close();
     return 0;
 }
 
-bool is_even_digit_sum(const char* str) {
+int is_even_digit_sum(const char* str) {
     int sum = 0;
     for (int i = 0; str[i] != '\0'; i++) {
-        sum += (int)str[i] - 48;
+        if (isdigit(str[i])) {
+            sum += str[i] - '0';
+        }
     }
     return (sum % 2 == 0);
 }
 
-// Генерация случайного числа в заданном диапазоне [min, max]
 int generate_random_int(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-// Поиск и вставка чисел в список, если узел соответствует условию
-void process_list(node* head) {
+void process_list(node*& head) {
     srand(time(NULL));
 
     node* prev = nullptr;
-    while (head != nullptr) {
-        if (is_even_digit_sum(head->info)) {
+    node* current = head;
+
+    while (current != nullptr) {
+        if (is_even_digit_sum(current->info)) {
             for (int i = 0; i < 3; i++) {
                 node* insert_nod = new node;
 
-                int rand_int = generate_random_int(1, 10);
+                int rand_int = generate_random_int(1, 9);
 
                 char buff[255];
                 itoa(rand_int, buff, 10);
                 strcpy(insert_nod->info, buff);
 
-                head = insert_node(head, prev, insert_nod);
+                if (prev == nullptr) {
+                    insert_nod->link = head;
+                    head = insert_nod;
+                }
+                else {
+                    insert_nod->link = prev->link;
+                    prev->link = insert_nod;
+                }
             }
         }
 
-        prev = head;
-        head = head->link;
+        prev = current;
+        current = current->link;
     }
 }
 
-
 void print_list(const node* first, fstream& ost) {
-    while (first != 0x0) {
-        char temp[255];
-        strcpy(temp, first->info);
-        ost << temp << " ";
+    while (first != nullptr) {
+        ost << first->info << " ";
         first = first->link;
     }
+    ost << endl; 
 }
 
 void push_back(node*& top, const char* symbol) {
-    char temp[255] = "";
-    strcpy(temp, symbol);
     if (top == nullptr) {
         top = new node;
-
-        strcpy(top->info, temp);
+        strcpy(top->info, symbol);
         top->link = nullptr;
     }
-
     else {
         node* k = top;
         while (k->link != nullptr) {
             k = k->link;
         }
-        k->link = new node;
-        strcpy(k->link->info, symbol);
-        k->link->link = nullptr;
+        node* new_node = new node;
+        strcpy(new_node->info, symbol);
+        new_node->link = nullptr;
+        k->link = new_node;
     }
 }
 
-void print_list_own(node* head_list) {
-    while (head_list != 0x0) {
+void print_list_own(node* head_list, fstream& ost) { 
+    while (head_list != nullptr) {
         if (head_list->link != nullptr) {
-            cout << head_list->info << " -> ";
+            ost << head_list->info << " ";
         }
         else {
-            cout << head_list->info << endl;
+            ost << head_list->info << endl;
         }
         head_list = head_list->link;
     }
 }
 
 void push_forward(node*& top, const char* symbol) {
-    char temp[255];
-    strcpy(temp, symbol);
-    if (top == nullptr) {
-        push_back(top, symbol);
-    }
-    else {
-        node* k = new node;
-        k->link = top;
-        strcpy(k->info, temp);
-        top = k;
-    }
+    node* k = new node;
+    strcpy(k->info, symbol);
+    k->link = top;
+    top = k;
 }
 
 node* insert_node(node* head, node* prev, node* n) {
-    if (prev == nullptr) {
+    if (prev == nullptr) {р
         push_forward(head, n->info);
     }
-
     else {
         n->link = prev->link;
         prev->link = n;
@@ -165,7 +158,6 @@ void fill_list(fstream& ist, node*& head) {
 
     while (ist.getline(temp, 244)) {
         str_tmp = strtok(temp, " ");
-        //cout << str_tmp << endl;
         push_back(head, str_tmp);
         while (str_tmp = strtok(NULL, " ")) {
             push_back(head, str_tmp);
@@ -173,23 +165,12 @@ void fill_list(fstream& ist, node*& head) {
     }
 }
 
-node* lstchr(node* head, const char* symbol) { // Analog of strchr
-    bool Flag{ false };
-    while (head->link != nullptr) {
+node* lstchr(node* head, const char* symbol) { // Аналог strchr
+    while (head != nullptr) {
         if (!strcmp(head->info, symbol)) {
-            Flag = true;
             return head;
-            break;
         }
         head = head->link;
     }
-    if (!strcmp(head->info, symbol)) { // Костыль   
-        Flag = true;
-        return head;
-    }
-    if (Flag == false) {
-        return 0x0;
-    }
+    return nullptr;
 }
-
-
